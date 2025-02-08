fa3514d7f49b92cab426a75a582b45f25158cb34
Bahrom Karimov <BKarimov7@gmail.com>
2025-02-08 17:04:05 +0500

    Переменая, консольный выввод

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,3 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
+// *** Тып данных имя переменной
+
+
+/* int a = 5;
+bool variable = false;
+bool Variable = true;
+double b = 1.33;
+char c = '#';
+string s = "Бахром";
+    
+Console.WriteLine(s);
+Console.WriteLine(c);
+Console.WriteLine(b);
+Console.WriteLine(a);
+Console.WriteLine(variable);
+Console.WriteLine(Variable);
+
+int i = 5;
+Console.WriteLine(i);
+
+i = 455;
+Console.WriteLine(i);
+*/
+
+
+// *** Консольный вывод
+
+/*
+string date;
+date = Console.ReadLine();
+Console.WriteLine("Привет " +  date + "!!!");
+Console.WriteLine($"Привет {date} !!!");
+
+
+
+string hallo = "Привет мир";
+Console.WriteLine(hallo);
+Console.WriteLine("Добро пожаловть в С#");
+Console.WriteLine("Пока мир ...");
+Console.WriteLine(24.5);
+
+string name = "Том";
+int age = 42;
+double height = 1.7;
+Console.WriteLine($"Имя: {name}, Возраст: {age}, Рост: {height}");
+Console.WriteLine("Имя: " + name + ", Возраст: " + age + ", Рост: " + height);
+Console.WriteLine("Имя: {0}, Возврат: {1}, Рост: {2}", name, age, height);
+*/
+
+// *** Консольный ввод
+
+Console.WriteLine("Введите своё имя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Привет " + name + "!");
+Console.WriteLine($"Привет {name}!");
