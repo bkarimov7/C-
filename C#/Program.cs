9bd07c2f73b1723773ce0d13b84f0ccb0e870be1
Bahrom Karimov <BKarimov7@gmail.com>
2025-02-11 18:49:42 +0500

    Инкремент и дикремет

--- C#/Program.cs
+++ C#/Program.cs
@@ -58,7 +58,7 @@ Console.WriteLine("Привет " + name + "!");
 Console.WriteLine($"Привет {name}!");
 */
 
-// Конвертация строки
+// *** Конвертация строки класс Convert
 
 /*
 int a, b;
@@ -86,5 +86,94 @@ decimal salary = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height} Зарплата: {salary}");
 */
+
+// *** Конвертация строки  
+//     методом parse и tryparse
+
+/*
+string str = "5,9";
+
+double a = double.Parse(str);
+
+Console.ReadKey();
+
+*/
+
+
+// *** Операторы
+// *** Арифметические операции с числами
+
+// Домашнее задание
+
+/*
+1.Напишите программу, вычисляющую среднее арифметическое двух чисел.
+*/
+
+/*
+Console.WriteLine("Введите певое число");
+double chislo_1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double chislo_2 = double.Parse(Console.ReadLine());
+
+double result = ((double) chislo_1 + chislo_2) / 2;
+
+Console.WriteLine($"Среднее двух чисель {result}");
+
+Console.ReadLine();
+*/
+
+// 2. Введите три числа и выведите на экран значение суммы и произведения этих чисел
+
+/*
+Console.WriteLine("Введите певое число");
+double chislo_1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double chislo_2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третью число");
+double chislo_3 = double.Parse(Console.ReadLine());
+
+double result1 = (chislo_1 + chislo_2 + chislo_3);
+double result2 = (chislo_1 * chislo_2 * chislo_3);
+
+Console.WriteLine($"Значения трех чисел равна  {result1}");
+Console.WriteLine($"Произведение трех чисел равна  {result2}");
+*/
+
+/*3.Напишите простой конвертер валют (без возможности динамического выбора валюты пользователем). Валюты заданы хардкодом и не изменяются.
+Тип валют на выбор программиста*/
+
+/*
+Console.WriteLine("Введите сумму продажи в валюте сомони: ");
+double chislo_1 = double.Parse(Console.ReadLine());
+
+double USD = 10.01;
+double EUR = 11.01;
+double RUR = 0.12;
+
+double result_USD = (chislo_1 / USD);
+double result_EUR = (chislo_1 / EUR);
+double result_RUR = (chislo_1 / RUR);
+
+Console.WriteLine($"Сумма покупки в Долларах равна: \n {result_USD}");
+Console.WriteLine($"Сумма покупки в Евро равна: \n {result_EUR}");
+Console.WriteLine($"Сумма покупки в Рублях равна: \n {result_RUR}");
+*/
+
+// Несколько проектов в одном решении
+
+
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
 
 
